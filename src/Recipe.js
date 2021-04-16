6167fe3704978ad643fea070cc1f0b3e9f68125c
phimanhhai01 <19020277@vnu.edu.vn>
2021-04-16 18:25:09 +0700

    á

---
+++ src/Recipe.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import style from "./recipe.module.css";
+const Recipe = ({title, calories, image, ingredients}) => {
+    return (
+        <div  className={style.recipe}>
+            <h1 className={style.title}>{title}</h1>
+            <ol>
+                {ingredients.map(ingredient => (
+                    <li>{ingredient.text}</li>
+                ))}
+            </ol>
+            <p>Calories: {calories}</p>
+            <img src={image} alt="" />
+        </div>
+    );
+};
+
+export default Recipe;
